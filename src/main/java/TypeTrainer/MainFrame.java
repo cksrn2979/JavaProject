1b5c85ba2622455350403eaf584096a141fcc2da
changoo <changoo@Changoo-pc>
2016-05-08 19:14:31 +0900

    최초
    최초

---
+++ src/main/java/TypeTrainer/MainFrame.java
@@ -0,0 +1,10 @@
+package TypeTrainer;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame{
+	public static void main(String[] args){
+		MainFrame mf= new MainFrame();
+	}
+}
---
+++ src/test/java/TypeTrainer/MainFrameTest.java
@@ -0,0 +1,14 @@
+package TypeTrainer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainFrameTest {
+	
+	@Test
+	public void newTest(){
+		MainFrame mf = new MainFrame();
+	}
+
+}
