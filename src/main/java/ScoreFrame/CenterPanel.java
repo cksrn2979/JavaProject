47029f36b3fba5dc51c3c20c87b9470621f48b4f
Song <Song@reindeer>
2016-05-30 01:40:51 +0900

    스코어패널

--- src/main/java/ScoreFrame/CenterPanel.java
+++ src/main/java/ScoreFrame/CenterPanel.java
@@ -22,18 +22,61 @@ import ScoreFrame.FileIO;
 class CenterPanel extends JPanel{
 	CenterPanel(){
 		setVisible(true);
-		setBackground(Color.gray);
+		setBackground(Interface.GameColor.lyan);
 		setLayout(null);
 		setPreferredSize(new Dimension(500,420));
 		
-		//CenterPanel 에서 함수호출 테스트 
-		Integer i=new Integer(ScorePanel.sp.fIO.getScore("s"));
-		JButton a=new JButton(i.toString());
-		a.setSize(100,100);
-		a.setLocation(100,100);
-		add(a);
+		String path="C:/Users/Song/git/JavaProject/images/ScoreFrame/CenterPanel/";
+		
+		int num=3;//전체 화면에 표시할 등수 갯수
+		
+		ImageIcon images[]=new ImageIcon[num];
+		ImageIcon gradeImg []=new ImageIcon[num];
+		
+		JLabel faceLabel[]=new JLabel[num];
+		JLabel scores[]=new JLabel[num];
+		JLabel gradeLabel[]=new JLabel[num];
+		JLabel nameLabel[]=new JLabel[num];
+		
+		String name="0";
+		
+			for(int i=0;i<num;i++){
+			
+			images[i]=new ImageIcon(path+"고양이얼굴.png");	
+			gradeImg[i]=new ImageIcon(path+"등수.png");
+					
+			faceLabel[i]=new JLabel(images[i]);
+			gradeLabel[i]=new JLabel(gradeImg[i]);
+			
+			name=ScorePanel.sp.fIO.getGradeName(i);
+			
+			nameLabel[i]=new JLabel(name);
+			scores[i]=new JLabel(ScorePanel.sp.fIO.getScore(name).toString());
+
+			faceLabel[i].setSize(100,100);
+			faceLabel[i].setLocation(100,100+i*100);
+			
+			gradeLabel[i].setSize(100,100);
+			gradeLabel[i].setLocation(200,100+i*100);
+			
+			nameLabel[i].setSize(100,100);
+			nameLabel[i].setLocation(300, 100+i*100);
+			
+			scores[i].setSize(100,100);
+			scores[i].setLocation(350, 100+i*100);
+		
+			add(faceLabel[i]);
+			add(gradeLabel[i]);
+			add(scores[i]);
+			add(nameLabel[i]);
+		}
 		
 		
+		
+		
+	
+	
+	
 	}
 }
 
